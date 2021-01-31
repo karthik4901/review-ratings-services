4b5ed058bf79c61796c0e8a77a5c85521de43a24
karth <karth@LAPTOP-3Q1JCPDS.lan>
2021-01-31 17:20:49 -0600

    first

---
+++ src/main/java/com/info/reviewratingsservice/ReviewRatingsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.info.reviewratingsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewRatingsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewRatingsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/info/reviewratingsservice/ReviewRatingsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.info.reviewratingsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewRatingsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
